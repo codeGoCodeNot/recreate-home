b1f9a120d1fbfaaab0f3c35284fe6383931ffcea
sendosend <johnsenberdin2930@gmail.com>
2025-10-21 06:26:17 +0800

    first commit

---
+++ my-project/src/components/Header.jsx
@@ -0,0 +1,52 @@
+import { RxHamburgerMenu } from "react-icons/rx";
+import { RxCross2 } from "react-icons/rx";
+import { useState } from "react";
+
+const navItems = ["HOME", "LISTINGS", "LET'S MOVE", "ABOUT US"];
+
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const handleClick = () => setMenuOpen(!menuOpen);
+  return (
+    <header className="flex items-center justify-center relative">
+      <button onClick={handleClick}>
+        <RxHamburgerMenu
+          className="fabars left-50 top-1/2 -translate-y-1/2 absolute cursor-pointer"
+          size={25}
+          color="#7d6e6e"
+        />
+      </button>
+      <img
+        className="marci-img cursor-pointer"
+        src="./src/assets/marci.png"
+        alt="Marci Metzger"
+      />
+      {menuOpen && (
+        <div className="fixed top-0 left-0 h-full w-[420px] bg-[#f6f6f6] z-1 ">
+          <div className="flex justify-between items-start px-8 pt-8">
+            <div></div>
+            <RxCross2
+              size={25}
+              className="cursor-pointer text-[#7d6e6e] absolute top-19 right-12"
+              onClick={() => setMenuOpen(false)}
+            />
+          </div>
+          <nav className="mt-12 px-8 flex flex-col gap-8 ml-12">
+            {navItems.map((item) => (
+              <span
+                key={item}
+                className={`text-[#7d6e6e]  text-sm cursor-pointer ${
+                  item === "HOME" ? "font-[550]" : "font-normal"
+                }`}
+              >
+                {item}
+              </span>
+            ))}
+          </nav>
+        </div>
+      )}
+    </header>
+  );
+};
+
+export default Header;
